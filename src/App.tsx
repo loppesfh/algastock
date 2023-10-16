4f319d5447493a50a7941fbb3f696494c75696d9
Lopes Filho <loppes@megganet.com.br>
2023-10-16 19:51:47 -0300

    add button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import "./App.css";
+import Button from "./components/Button";
 import Header from "./components/Header";
+
+function TestComponent () {
+  return <img width="16" src="https://images.freeimages.com/fic/images/icons/1053/project/256/search_find.png"
+  alt="Pesquisar" />
+}
 
 function App() {
   return (
     <div className="App">
-      <Header title="AlgaStock" />  
-      App
+      <Header title="AlgaStock" />
+      <div className="Container">
+        <Button 
+          onClick={() => window.alert("fçdjfçalj")}
+          appendIcon={<TestComponent/>}
+          >
+          Alert
+          </Button>
+      </div>
     </div>
   );
 }
