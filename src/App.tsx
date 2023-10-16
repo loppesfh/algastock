4f1dcf4646b563ca67271043695794d4a3ae91b9
Lopes Filho <loppes@megganet.com.br>
2023-10-16 20:07:02 -0300

    add container and moving button to shared

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import "./App.css";
-import Button from "./components/Button";
+import Button from "./assets/shared/Button";
+import Container from "./assets/shared/Container";
 import Header from "./components/Header";
 
-function TestComponent () {
-  return <img width="16" src="https://images.freeimages.com/fic/images/icons/1053/project/256/search_find.png"
-  alt="Pesquisar" />
+function TestComponent() {
+  return (
+    <img
+      width="16"
+      src="https://images.freeimages.com/fic/images/icons/1053/project/256/search_find.png"
+      alt="Pesquisar"
+    />
+  );
 }
 
 function App() {
   return (
     <div className="App">
       <Header title="AlgaStock" />
-      <div className="Container">
-        <Button 
+      <Container>
+        <Button
           onClick={() => window.alert("fçdjfçalj")}
-          appendIcon={<TestComponent/>}
-          >
+          appendIcon={<TestComponent />}
+        >
           Alert
-          </Button>
-      </div>
+        </Button>
+      </Container>
     </div>
   );
 }
